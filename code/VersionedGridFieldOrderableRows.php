b9c49d4bf97853c3a1f736ff01f2e8a9fb6b1fd6
Ben Dubuisson <ben.dubuisson@heyday.co.nz>
2015-11-17 09:57:17 +1300

    calling cache-include invalidate hook

--- code/VersionedGridFieldOrderableRows.php
+++ code/VersionedGridFieldOrderableRows.php
@@ -378,7 +378,7 @@ class VersionedGridFieldOrderableRows extends RequestHandler implements
         $class = singleton($className);
         if ($class->has_extension('Heyday\CacheInclude\SilverStripe\InvalidationExtension')) {
             $dataobject = $class::get()->first();
-            $dataobject->onChange('write');
+            $dataobject->extend('onAfterReorder');
         }
     }
 
